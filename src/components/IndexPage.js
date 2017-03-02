ec2ebfcf7a83f3242a5f740ac072843e2221f228
silvanagv <silvanagoberdhanvigle@gmail.com>
2017-03-02 09:56:31 -0500

    spacing

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -5,9 +5,9 @@ class IndexPage extends React.Component {
     return (
       <div>
         <h1> Food Deserts </h1>
-        <div className="fixed-width">
-          <img alt="food desert" className="food-desert" src={require('./../food-desert-bw-sm.jpg')}/>
-          <p className="definition"> The USDA defines a food desert as a “low-access community,” at least 500 people and/or at least 33 percent of the census tract's population must reside more than one mile from a supermarket or large grocery store (for rural census tracts, the distance is more than 10 miles). </p>
+          <div className="fixed-width">
+            <img alt="food desert" className="food-desert" src={require('./../food-desert-bw-sm.jpg')}/>
+            <p> The USDA defines a food desert as a “low-access community,” at least 500 people and/or at least 33 percent of the census tract's population must reside more than one mile from a supermarket or large grocery store (for rural census tracts, the distance is more than 10 miles). </p>
         </div>
       </div>
     );
--- src/components/ProblemPage.js
+++ src/components/ProblemPage.js
@@ -7,8 +7,6 @@ class ProblemPage extends Component {
         <h1> The Problem </h1>
         <div className="fixed-width">
           <img alt="Halal Food Cart" className="food-desert" src={require('./../halal-food-sm.jpg')}/>
-          <img alt="Half-Eaten Doughnuts" className="food-desert" src={require('./../doughnuts-sm.jpg')}/>
-
           <p> "Food deserts" are typically low-income areas that have virtually no supermarkets. The closest thing to a grocery store, especially in urban areas, is often a deli or bodega. Studies have shown that these "deserts" are associated with higher levels of obesity and malnutrition, which can affect entire communities, including small children. Unsurprisingly, food deserts are also usually plagued by poverty.</p>
         </div>
       </div>
